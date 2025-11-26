8eb5318fcb6c7d1080c69370c317b6a9392eb2cf
carlosq-mv <cquiroz7869@gmail.com>
2025-11-26 08:27:25 -0800

    feat(schema): add UserCreateSchema and UserLoginSchema

--- app/schema/user_schema.py
+++ app/schema/user_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 class UserSchema(BaseModel):
+    id: int
     username: str
     name: str
     email: str
@@ -10,3 +11,13 @@ class UserUpdateSchema(BaseModel):
     username: str | None = None
     name: str | None = None
     email: str | None = None
+
+class UserCreateSchema(BaseModel):
+    username: str
+    name: str 
+    email: str
+    password: str
+
+class UserLoginSchema(BaseModel):
+    email: str
+    password: str
