2e82809625744d00869f658830ce8639050dd510
carlosq-mv <cquiroz7869@gmail.com>
2025-11-25 21:43:08 -0800

    feat(api): add main API router and register v1 endpoints

---
+++ app/api/v1/api.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from app.api.v1.user_api import router as user_router
+
+api_router = APIRouter()
+
+api_router.include_router(user_router, prefix="/users", tags=["Users"])
