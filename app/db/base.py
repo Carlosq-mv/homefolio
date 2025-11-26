c356aef815317e4c0227739704b5f1854d8f5c1f
carlosq-mv <cquiroz7869@gmail.com>
2025-11-25 21:41:27 -0800

    feat(db): configure SQLAlchemy engine and SessionLocal

---
+++ app/db/base.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker     
+from app.core.config import get_env
+
+engine = create_engine(get_env().DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
