5168c4c111fa7e6a78e87f03cb5e3356ff82e1f8
carlosq-mv <cquiroz7869@gmail.com>
2025-11-26 08:28:45 -0800

    chore(logging): initialize application logging in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import logging
+
 from app.api.v1.api import api_router
 from app.db.base import Base, engine
 from app.core.exceptions import UserError
+from app.core.logging_config import setup_logging
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -10,6 +17,8 @@ Base.metadata.create_all(bind=engine)
 
 @app.exception_handler(UserError)
 async def user_error_handler(_, exc: UserError):
+    logger.warning(f"{exc.__class__.__name__}-{exc.message}(code={exc.status_code})")
+
     return JSONResponse(
         status_code=exc.status_code,
         content={"detail" : exc.message}
