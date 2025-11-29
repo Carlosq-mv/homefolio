a24e0806a31417240aa032c2aeadd2ab3898f6cc
carlosq-mv <cquiroz7869@gmail.com>
2025-11-29 07:29:36 -0800

    feat(api): add POST /login endpoint and tweak /create-user endpoint

--- app/api/v1/user_api.py
+++ app/api/v1/user_api.py
@@ -10,28 +10,38 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-@router.post("/create-user")
+@router.post("/create-user", response_model=UserSchema, status_code=status.HTTP_201_CREATED)
 async def create_user(
     user: UserCreateSchema,
-    service: UserService = Depends(get_user_services)
+    service: UserService = Depends(get_user_services),
 ) -> UserSchema:
     try:
         logger.info(f"Creating a new user.")
         return service.create_user(user)
     except IntegrityError as e:
-        logger.error(f"Integrity error occurred: {str(e)} ")
+        logger.error(f"Integrity error occurred: ", exc_info=(type(e), e, e.__traceback__.tb_next))
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, 
             detail="Database integrity error"
         )
     except SQLAlchemyError as e:
-        logger.error(f"SQL error: {str(e)}")
+        logger.error(f"SQL error: ", exc_info=(type(e), e, e.__traceback__.tb_next))
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
             detail="A database error occurred."
         )
 
 
-@router.post("/login")
-async def login(user: UserLoginSchema):
-    pass
+@router.post("/login", response_model=dict[str, str], status_code=status.HTTP_202_ACCEPTED)
+async def login(
+    login_data: UserLoginSchema,
+    service: UserService = Depends(get_user_services)
+) -> dict[str,str]:
+    try:
+        return service.login(login_data)
+    except AttributeError as e:
+        logger.warning("Attribute Error: ", exc_info=(type(e), e, e.__traceback__.tb_next))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error occurred."
+        )
