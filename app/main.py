436d63e4d2c512129e8fb219a5cede53312e21ab
carlosq-mv <cquiroz7869@gmail.com>
2025-11-29 07:35:31 -0800

    feat: add global exception handling for user defined JwtError exceptions

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import logging
 
 from app.api.v1.api import api_router
 from app.db.base import Base, engine
-from app.core.exceptions import UserError
+from app.core.exceptions import UserError, JwtError
 from app.core.logging_config import setup_logging
 
 setup_logging()
@@ -17,6 +17,15 @@ Base.metadata.create_all(bind=engine)
 
 @app.exception_handler(UserError)
 async def user_error_handler(_, exc: UserError):
+    logger.warning(f"{exc.__class__.__name__}-{exc.message}(code={exc.status_code})")
+
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail" : exc.message}
+    )
+
+@app.exception_handler(JwtError)
+async def user_error_handler(_, exc: JwtError):
     logger.warning(f"{exc.__class__.__name__}-{exc.message}(code={exc.status_code})")
 
     return JSONResponse(
