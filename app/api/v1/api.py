b8ef788d4b5929454e06efb06b9ebcd7b53ab1b6
carlosq-mv <cquiroz7869@gmail.com>
2025-11-29 07:30:17 -0800

    feat(api): add token API router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.api.v1.user_api import router as user_router
+from app.api.v1.jwt_api import router as jwt_router
 
 api_router = APIRouter()
 
 api_router.include_router(user_router, prefix="/users", tags=["Users"])
+api_router.include_router(jwt_router, prefix="/token", tags=["JWT Tokens"])
