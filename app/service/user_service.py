598aa58f81237bfd1c93d5dc993a8f1628252622
carlosq-mv <cquiroz7869@gmail.com>
2025-11-29 07:34:58 -0800

    feat(service): add login method with jwt tokens to UserService

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,13 +1,41 @@
 from sqlalchemy.orm import Session
 
 from app.crud.user_crud import UserCRUD
-from app.schema.user_schema import UserSchema, UserUpdateSchema
+from app.schema.user_schema import UserSchema, UserUpdateSchema, UserLoginSchema
 from app.model.user import User
-from app.core.exceptions import UserEmailExists, UserUsernameExists, UserDoesNotExist, NoUsersExists, UserUsernameAndEmailExists
+from app.core.exceptions import UserEmailExists, UserUsernameExists, UserDoesNotExist, NoUsersExists, UserUsernameAndEmailExists, UserInvalidCredentials
+from app.core.utils.auth import Auth
+from app.service.jwt_service import JWTService
 
 class UserService:
     def __init__(self, db: Session):
         self.crud = UserCRUD(db=db)
+        self.auth = Auth()
+        self.jwt = JWTService(db=db)
+
+    def login(self, login_data: UserLoginSchema) -> dict[str, str]:
+        # check if there is a user with the email
+        user: User | None = self.crud.get_user_by_email(user_email=login_data.email)
+
+        if not user:
+            raise UserInvalidCredentials()
+
+        # check if the password is correct
+        if not self.auth.verify_password(
+            plain_password=login_data.password,
+            hashed_password=user.password
+        ):
+            raise UserInvalidCredentials()
+        
+        # create the token pair (access & refresh token)
+        refresh_token: str = self.jwt.create_refresh_token(user_id=user.id)
+        access_token: str = self.jwt.create_access_token(user_id=user.id)
+
+        # return the tokens
+        return {
+            "access" : access_token,
+            "refresh" : refresh_token
+        }
 
     def create_user(self, user: UserSchema) -> User:
         existing_email: User | None = self.crud.get_user_by_email(user_email=user.email)
